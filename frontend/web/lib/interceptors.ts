222f0c1a988e6217ffb2c22c5dbcae68bba738e3
Thanh Tri <phamthanhtri0712@gmail.com>
2026-06-24 22:11:53 +0700

    feature(auth): adding interceptors

---
+++ frontend/web/lib/interceptors.ts
@@ -0,0 +1,14 @@
+import { api } from "./axios";
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
