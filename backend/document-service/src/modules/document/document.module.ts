fed2e2610a502946e504fda8b49e7fd02936b359
Thanh Tri <phamthanhtri0712@gmail.com>
2026-08-01 16:17:52 +0700

    feat(documents): implement trash item automatic clean after 30 days

--- backend/document-service/src/modules/document/document.module.ts
+++ backend/document-service/src/modules/document/document.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
 import { QuotaModule } from '../quota/quota.module';
 import { S3Module } from '../../infrastructure/s3/s3.module';
+import { TrashCleanupService } from './trash-cleanup.service';
 
 @Module({
-  imports: [QuotaModule, S3Module],
+  imports: [ScheduleModule.forRoot(), QuotaModule, S3Module],
   controllers: [DocumentController],
-  providers: [DocumentService],
+  providers: [DocumentService, TrashCleanupService],
   exports: [DocumentService],
 })
 export class DocumentModule {}
